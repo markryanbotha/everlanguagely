2b6b5c8b5ef868cb51a8e31fc268dcf5d7f013c2
Mark Botha <bothamar@amazon.com>
2021-01-25 18:24:52 +0000

    Functionality: Show Translation when clicked

--- client/src/components/notReviewed.jsx
+++ client/src/components/notReviewed.jsx
@@ -1,6 +1,6 @@
 import { gql, useQuery } from "@apollo/client"
 import { Link } from "react-router-dom"
-import { React, useState } from "react"
+import { React, useState, useRef } from "react"
 
 const GET_ALL_WORDS = gql`
   query {
@@ -19,8 +19,8 @@ export default function NotReviewed() {
   const { loading, error, data } = useQuery(GET_ALL_WORDS)
   var search = ""
   const [myQuery, setMyQuery] = useState("")
-
-  console.log(data)
+  const [trigger, setTrigger] = useState(false)
+  const indexOfTranslations = useRef([])
 
   if (loading) {
     return (
@@ -37,19 +37,30 @@ export default function NotReviewed() {
       obj["translation"].toLowerCase().includes(myQuery.toLowerCase())
   )
 
-  // Object.values(data.getAllWords).join().toLowerCase().includes(myQuery.toLowerCase())
   const getSearch = (e) => {
     e.preventDefault()
     setMyQuery(search)
   }
 
+  const handleCardClick = (e, index) => {
+    e.preventDefault()
+    indexOfTranslations.current.push(index)
+    setTrigger(!trigger)
+    console.log(indexOfTranslations.current)
+  }
+
+  const cardStyle = (index) => ({
+    color: "#677ea3",
+    visibility: indexOfTranslations.current.includes(index) ? "visible" : "collapse",
+  })
+
   //filter before passing to map
-  const myData = wordsAfterSearch.map(({ id, word, translation }) => (
+  const myData = wordsAfterSearch.map(({ id, word, translation }, index) => (
     <div className="card card-body mb-3" key={id}>
       <div className="row">
-        <div className="col-md-9">
+        <div className="col-md-9" onClick={(e) => handleCardClick(e, index)}>
           <h4>{word}</h4>
-          <h4 style={{ color: "#677ea3" }}> {translation}</h4>
+          <h4 style={cardStyle(index)}> {translation}</h4>
         </div>
         <div className="col md-3">
           <Link to={`/word/${id}`} className="btn btn-secondary">
