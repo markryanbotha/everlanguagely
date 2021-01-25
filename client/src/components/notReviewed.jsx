a1631d42f24fba86dc24227480c5cf9857f34201
Mark Botha <bothamar@amazon.com>
2021-01-25 20:50:15 +0000

    Refactored: Showing Translation when you click

--- client/src/components/notReviewed.jsx
+++ client/src/components/notReviewed.jsx
@@ -1,6 +1,6 @@
 import { gql, useQuery } from "@apollo/client"
 import { Link } from "react-router-dom"
-import { React, useState, useRef } from "react"
+import { React, useState } from "react"
 
 const GET_ALL_WORDS = gql`
   query {
@@ -19,8 +19,7 @@ export default function NotReviewed() {
   const { loading, error, data } = useQuery(GET_ALL_WORDS)
   var search = ""
   const [myQuery, setMyQuery] = useState("")
-  const [trigger, setTrigger] = useState(false)
-  const indexOfTranslations = useRef([])
+  const [isTranslated, setIsTranslated] = useState({})
 
   if (loading) {
     return (
@@ -42,28 +41,31 @@ export default function NotReviewed() {
     setMyQuery(search)
   }
 
-  const handleCardClick = (e, index) => {
+  const handleCardClick = (e, id) => {
     e.preventDefault()
-    indexOfTranslations.current.push(index)
-    setTrigger(!trigger)
-    console.log(indexOfTranslations.current)
+    if (isTranslated[id] === true) {
+      setIsTranslated({ ...isTranslated, [id]: false })
+    } else {
+      setIsTranslated({ ...isTranslated, [id]: true })
+    }
   }
 
-  const cardStyle = (index) => ({
+
+  const translationStyle = (id) => ({
     color: "#677ea3",
-    visibility: indexOfTranslations.current.includes(index) ? "visible" : "collapse",
+    visibility: isTranslated[id] === true ? "visible" : "collapse",
   })
 
   //filter before passing to map
-  const myData = wordsAfterSearch.map(({ id, word, translation }, index) => (
-    <div className="card card-body mb-3" key={id}>
+  const myData = wordsAfterSearch.map(({ id, word, translation }) => (
+    <div className="card card-body mb-3">
       <div className="row">
-        <div className="col-md-9" onClick={(e) => handleCardClick(e, index)}>
-          <h4>{word}</h4>
-          <h4 style={cardStyle(index)}> {translation}</h4>
+        <div className="col-md-9" onClick={(e) => handleCardClick(e, id)}>
+          <h1>{word}</h1>
+          <h1 style={translationStyle(id)}> {translation}</h1>
         </div>
         <div className="col md-3">
-          <Link to={`/word/${id}`} className="btn btn-secondary">
+          <Link to={`/word/${id}`} className="btn btn-secondary float-right">
             Details
           </Link>
         </div>
@@ -76,7 +78,7 @@ export default function NotReviewed() {
       <h1 className="display-4 my-3">Everlanguagely</h1>
       <form onSubmit={(e) => getSearch(e)} className="form-inline mb-4">
         <input
-          className="form-control mr-sm-2"
+          className="col-auto"
           style={{ width: "1028px", height: 36 }}
           type="text"
           onChange={(e) => (search = e.target.value)}
