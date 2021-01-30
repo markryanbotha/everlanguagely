348214774db8fb064e7d94eb6f6a532da9baac75
Mark Botha <bothamar@amazon.com>
2021-01-30 21:07:53 +0000

    Refactor: Now that state is tracked separately in the new component, I can use a boolean rather than an array

--- client/src/components/WordCards.jsx
+++ client/src/components/WordCards.jsx
@@ -2,28 +2,24 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 export const WordCards = ({ id, word, translation }) => {
-  const [isTranslated, setIsTranslated] = useState({})
+  const [isTranslated, setIsTranslated] = useState(false)
 
   const handleCardClick = (e, id) => {
     e.preventDefault()
-    if (isTranslated[id] === true) {
-      setIsTranslated({ ...isTranslated, [id]: false })
-    } else {
-      setIsTranslated({ ...isTranslated, [id]: true })
-    }
+    setIsTranslated(!isTranslated)
   }
 
   const translationStyle = (id) => ({
     color: "#677ea3",
-    visibility: isTranslated[id] === true ? "visible" : "collapse",
+    visibility: isTranslated === true ? "visible" : "collapse",
   })
 
   return (
     <div className="card card-body mb-3">
       <div className="row">
-        <div className="col-md-9" onClick={(e) => handleCardClick(e, id)}>
+        <div className="col-md-9" onClick={handleCardClick}>
           <h1>{word}</h1>
-          <h1 style={translationStyle(id)}> {translation}</h1>
+          <h1 style={translationStyle()}> {translation}</h1>
         </div>
         <div className="col md-3">
           <Link to={`/word/${id}`} className="btn btn-secondary float-right">
